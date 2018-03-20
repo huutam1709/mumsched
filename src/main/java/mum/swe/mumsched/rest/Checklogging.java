69b451761323e0e07000cf3b94aaafef6fb7399d
yangmengjiao <mengjiaoshun@gmail.com>
2018-03-20 16:56:20 -0500

    commit aop relative code

--- src/main/java/mum/swe/mumsched/rest/Checklogging.java
+++ src/main/java/mum/swe/mumsched/rest/Checklogging.java
@@ -1,22 +1,25 @@
 package mum.swe.mumsched.rest;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
 
 @Aspect   
+@Component
 public class Checklogging {  
-  
-    @Pointcut("execution(* mum.swe.mumsched.controller.*signin(..))")  
-    public void checkLogging(){  
-        System.out.println("**************The System is logging by someone****************");  
-    }  
-      
-   
-    @Before("checkLogging()")  
-    public void beforeCheck(){  
-        System.out.println(">>>>>>>> Start do logging ..........");  
-    }  
-      
-   
+   /*
+    * Maybe you don't need to define pointcuts, but instead just define the advices as this one below
+    * and it should take care of inserting
+    */
+	@Before("execution(* mum.swe.mumsched.rest..*(..))")
+	public void checkLogging() {
+		System.out.println("**************The system is logging someone*****************");
+	}
+	
+	@After("execution(* mum.swe.mumsched.rest..*(..))")
+	public void checkLoggingAfter() {
+		System.out.println("**************Someone just finished something******************");
+	}
 }  
